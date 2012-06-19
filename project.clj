e0e4d79490c178264e52a2ab419a0f9141969c7c
Josh and Dan <eng@relaynetwork.com>
2012-06-19 17:21:51 -0400

    lein-release plugin: preparing 1.0.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-crypto "1.0.3-SNAPSHOT"
+(defproject clj-crypto "1.0.3"
   :description "Crypogrphy utilities"
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :local-repo-classpath true
