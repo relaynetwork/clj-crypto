0376479aa79463c48bab36b97b8062ddbb92ae6b
Pat and Kyle <kburton@relaynetwork.com>
2013-11-18 11:10:13 -0500

    fix ns

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-crypto "1.0.9"
+(defproject com.relaynetwork/clj-crypto "1.0.9"
   :description "Crypogrphy utilities"
   :local-repo-classpath true
   :global-vars {*warn-on-reflection* true}
