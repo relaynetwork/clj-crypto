132e6cc94b9422acdaeda3fc3387f84966859601
Pat and Kyle <kburton@relaynetwork.com>
2013-11-18 11:10:34 -0500

    burn version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/clj-crypto "1.0.9"
+(defproject com.relaynetwork/clj-crypto "1.0.10-SNAPSHOT"
   :description "Crypogrphy utilities"
   :local-repo-classpath true
   :global-vars {*warn-on-reflection* true}
