bdfc7ff495828f1f7293fb35583a5a5864121195
Josh and Kyle <kburton@relaynetwork.com>
2013-11-21 12:19:51 -0500

    lein-release plugin: bumped version from 1.0.13 to 1.0.14-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/clj-crypto "1.0.13"
+(defproject com.relaynetwork/clj-crypto "1.0.14-SNAPSHOT"
   :description "Crypogrphy utilities"
   :local-repo-classpath true
   :global-vars {*warn-on-reflection* true}
